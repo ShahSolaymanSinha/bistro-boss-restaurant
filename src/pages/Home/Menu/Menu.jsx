32893820d2c03fd86e33f1c2cf5c7b8e8a1fd51a
ShahSolaymanSinha <md.sinha.p@gmail.com>
2023-05-24 05:31:07 +0600

    update menu

--- src/pages/Home/Menu/Menu.jsx
+++ src/pages/Home/Menu/Menu.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import { useNavigate } from "react-router-dom";
 
 const Menu = () => {
     const [data, setData] = useState([]);
     const [isExpanded, setIsExpanded] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetch("menu.json")
@@ -15,14 +17,17 @@ const Menu = () => {
     return (
         <div className="mb-20">
             <SectionTitle heading={"From our menu"} subHeading={"Check it out"}></SectionTitle>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-x-20 gap-y-10">
+            <div id="menu" className="grid grid-cols-1 md:grid-cols-2 gap-x-20 gap-y-10">
                 {isExpanded == false
                     ? data.slice(0, 6).map((data) => <MenuItem key={data?._id} data={data}></MenuItem>)
                     : data.map((data) => <MenuItem key={data?._id} data={data}></MenuItem>)}
             </div>
             <div className="flex justify-center mt-5">
                 <button
-                    onClick={() => setIsExpanded(!isExpanded)}
+                    onClick={() => {
+                        setIsExpanded(!isExpanded);
+                        navigate("#menu");
+                    }}
                     className="btn bg-transparent text-black border-b-4 border-t-0 border-l-0 border-r-0 hover:text-white">
                     {isExpanded == false ? "View Full Menu" : "View Less Menu"}
                 </button>
