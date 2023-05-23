30494af9d867b55381c674a91443076c45841b79
ShahSolaymanSinha <md.sinha.p@gmail.com>
2023-05-24 04:57:55 +0600

    json data added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "../Banner/Banner";
 import Banner2 from "../Banner2/Banner2";
 import Category from "../Category/Category";
+import Menu from "../Menu/Menu";
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
             <Banner></Banner>
             <Category></Category>
             <Banner2></Banner2>
+            <Menu></Menu>
         </div>
     );
 };
---
+++ src/pages/Home/Menu/Menu.jsx
@@ -0,0 +1,12 @@
+import SectionTitle from "../../../components/SectionTitle";
+
+const Menu = () => {
+    return (
+        <div>
+            <SectionTitle heading={"From our menu"} subHeading={"Check it out"}></SectionTitle>
+            
+        </div>
+    );
+};
+
+export default Menu;
